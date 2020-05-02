ab60d441767a8d39d047ebf59eea517a9c8433c2
YitaoLiu1996 <62963998+YitaoLiu1996@users.noreply.github.com>
2020-05-02 18:38:31 -0400

    NBA api-keys file

---
+++ NBA api-keys.R
@@ -0,0 +1,8 @@
+url18 <- 'http://www.espn.com/nba/salaries/_/year/2019'
+url19 <- 'http://www.espn.com/nba/salaries'
+urlslry <- 'https://www.basketball-reference.com/contracts/players.html'
+statsUrl1head <- 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=200'
+statsUrl1tail <- '&SeasonType=Regular+Season&StatCategory=PTS'
+url0910 <- 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2009-10&SeasonType=Regular+Season&StatCategory=PTS'
+statsUrl2head <- 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=20'
+statsUrl2tail <- '&SeasonType=Regular+Season&StatCategory=PTS'
